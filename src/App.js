259109a0b391b92a5b532a5907762a02a48b52eb
Darnell <darnellfoster12@gmail.com>
2023-08-30 02:12:48 -0400

    Deleted some files that weren't needed

--- src/App.js
+++ src/App.js
@@ -9,14 +9,22 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+
+        <div className="Search-bar">
+          <button>Search</button>
+          <input type="text" id="Search" name="Search"></input>
+
+        </div>
+        
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
