0dacf4b444b758fc5017cf71e7ffc828a7e1cba7
Darnell <darnellfoster12@gmail.com>
2023-08-31 01:51:17 -0400

    Changed look for website

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import {API_ID, API_KEY} from './key';
 
 function App() {
+  let query = ""
+  let recipe_url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}`;
+// https://api.edamam.com/api/recipes/v2?type=public&q=cookie&app_id=c74766d1&app_key=f29f0fad4ba55d635f7b2db632946707
+
+  
+  /*
+   * Uses the text field from search as a query for the API URL above
+   * Retrives a json file from the link
+  */
+  function getRecipe(){
+    //request data from a server. The request can be of any type of API that returns the data in JSON
+    let result = fetch(recipe_url);
+  
+    console.log(result);
+  }
+
+
+  //The componet being returned and rendered
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <div className="Search-bar">
-          <button>Search</button>
-          <input type="text" id="Search" name="Search"></input>
-
-        </div>
-        
-
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <h2>
+          Cook-Book
+        </h2>
       </header>
+
+      <body className="App-body">
+
+        <form className='search_field'>
+          <input type="text" id="Search" name="Search" placeholder="Enter Ingridient"></input>
+          <button type="button" name="submit" onClick={getRecipe}>Search</button>
+        </form>
+
+
+      </body>
+
+
+      {/* <footer>
+        <p>
+          Darnell Foster
+        </p>
+      </footer> */}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'; // imports the main React library
+import ReactDOM from 'react-dom/client'; //imports a part of the react-dom package, responsible for rendering React components into the DOM (Document Object Model).
+import './index.css'; //imports a CSS file named "index.css" 
+import App from './App'; //imports the "App" component from "App.js"
 
+
+/*
+creates a "root" using the createRoot method from the react-dom package. 
+The createRoot function is used for the new Concurrent Mode in React.
+It takes an element (in this case, an element with the ID "root") as an argument and prepares it for rendering a React component.
+*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+renders a React component into the previously created root. 
+The component being rendered is wrapped in <React.StrictMode>, 
+which is a feature that helps highlight potential problems in your application during development. 
+The component being rendered is the "App" component.
+*/
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
